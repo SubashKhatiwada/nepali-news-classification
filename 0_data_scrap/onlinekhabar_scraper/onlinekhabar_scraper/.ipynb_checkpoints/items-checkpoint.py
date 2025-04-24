a680adbaf362cfaed69b439173f2628279644428
Subash Khatiwada <mr.subashkhatiwada@gmail.com>
2025-04-24 15:25:10 +0545

    rearranging files

--- 0_data_scrap/onlinekhabar_scraper/onlinekhabar_scraper/.ipynb_checkpoints/items-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class OnlinekhabarItem(scrapy.Item):
-    title = scrapy.Field()
-    url = scrapy.Field()
-    category = scrapy.Field()
-    image = scrapy.Field()
-    description = scrapy.Field()  # Optional, if available
